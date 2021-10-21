168fe4a338950fbc02333cd33972fb5b831b63c9
Hafiz VJ <38070777+Hafizvj@users.noreply.github.com>
2021-10-21 12:19:09 +0530

    port update

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -3333,3 +3333,16 @@
     });
   }
 })(jQuery);
+$( document ).ready(function () {
+    $(".moreBox").slice(0, 3).show();
+      if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+      }   
+      $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 6).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+          $("#loadMore").fadeOut('slow');
+        }
+      });
+    });
